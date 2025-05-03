5fb550a2aada741b75edf56a7c04932fbab7a0d7
Roberto Tiella <rtiella@gmail.com>
2025-05-03 11:42:58 +0200

    improve file example

--- samples/files.fs
+++ samples/files.fs
@@ -2,19 +2,36 @@
 : FN $" prova.fs" ;
 : W $" hello world!!!\n" ;
 VARIABLE FD
+VARIABLE BUF 100 ALLOT
 
-: RUN FN count w/o create-file 0= IF 
+: CLOSE-FILE-MSG  ( n -- )
+		close-file 0= IF
+		." close done!!!" CR
+		ELSE
+		." close error:" . CR
+		THEN ;
+
+: TEST-CREATE-FILE FN count w/o create-file 0= IF 
 	>R W count R@ write-file 0= IF
 		." write done!!!" CR
 		ELSE
 		." write error:" . CR
 		THEN
-		R> close-file 0= IF
-		." close done!!!" CR
-		ELSE
-		." close error:" . CR
-		THEN
+		R> CLOSE-FILE-MSG
 	ELSE
 	." open error:" . CR
 	THEN ;
-RUN
+
+: TEST-READ-FILE FN count r/o open-file 0= IF
+	." file opened!" CR
+	>R BUF 10 R@ read-file 0= IF
+			." buffer read, len: " . CR
+		ELSE
+			." cannot read buffer, reason:" . CR
+		THEN
+		R> CLOSE-FILE-MSG 
+	ELSE
+	." cannot open file, error:" . CR
+	THEN
+	 ;
+
